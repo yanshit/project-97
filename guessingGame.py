483bdee200260ab52429333717b28524fd1c3a06
yanshit <73741830+yanshit@users.noreply.github.com>
2021-07-26 17:10:40 +0530

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,21 @@
+import random
+import math 
+
+class GenerateRandom {
+    public static void main( String args[] ) {
+      int min = 1;
+      int max = 9;
+        
+     
+      System.out.println("Random value in int from "+min+" to "+max+ ":");
+      int random_int = (int)Math.floor(Math.random()*(max-min+1)+min);
+      System.out.println(random_int);
+    }
+}
+while chances < 5:
+
+if guess ==number:
+    print ("congratulation you won!😀")
+    break 
+if not chances < 5:
+    print("you lost !😥 the number is ",number)
